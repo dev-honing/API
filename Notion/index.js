9b7be3b1c545adc8fb8b57deee55bdfbac0e6bbb
Ho Nyeong <dev.honing@gmail.com>
2024-02-25 09:25:13 +0900

    Package: dotenv 설치 및 적용과 .gitignore 세팅
    - npm install dotenv

--- Notion/index.js
+++ Notion/index.js
@@ -1,11 +1,16 @@
+// index.js
+const dotenv = require('dotenv');
 const { Client } = require('@notionhq/client');
+
+// dotenv 설정
+dotenv.config();
 
 // 액세스 토큰 설정
 const notion = new Client({
-  auth: 'access_token',
+  auth: process.env.NOTION_TOKEN,
 });
 
 // 데이터 요청
 const response = await notion.databases.query({
-  database_id: 'database_id',
+  database_id: process.env.DATABASE_ID,
 });
